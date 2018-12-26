04b5112969223233765bebd5c8b3aa2425d3329a
Fei Fang <feifang@stanford.edu>
2018-12-26 22:07:30 +0800

    now can translate comments (in Javascript)

--- translate_code.py
+++ translate_code.py
@@ -1,12 +1,29 @@
 from google.cloud import translate
 from pyjsparser import PyJsParser
+import js2py
 import re
+import os
 
-p = PyJsParser()
+'''
+For now, assumes code is written in Javascript
+'''
+
+p = js2py.require("esprima")
+#p = PyJsParser()
 translate_client = translate.Client()
 
+def translate_text(text, target_language):
+	return translate_client.translate(text, target_language=target_language)['translatedText']
+
 def get_tree(string):
-	return p.parse(string)
+	return p.parse(string, {"comment": True}).to_dict()
+
+def get_comments(tree):
+	result = set([])
+	comment_entries = tree['comments']
+	for entry in comment_entries:
+		result.add(entry['value'])
+	return result
 
 def get_children(tree):
 	children = []
@@ -30,7 +47,7 @@ def get_translatables(tree, id_list, str_lit_list):
 			get_translatables(child, id_list, str_lit_list)
 
 
-def get_target(id_list, str_lit_list, target_language):
+def get_target(id_list, str_lit_list, comment_list, target_language):
 	src_target_map = dict([])
 
 	for source in id_list:
@@ -39,9 +56,10 @@ def get_target(id_list, str_lit_list, target_language):
 		target = concat(target)
 		src_target_map[source] = target
 
-	for source in str_lit_list:
+	for source in (str_lit_list | comment_list):
 		target = translate_client.translate(source, target_language=target_language)['translatedText']
 		src_target_map[source] = target
+
 	return src_target_map
 
 def src_to_target(code_src, src_target_map):
@@ -74,20 +92,21 @@ def concat(string):
 		substrings[i] = substrings[i].capitalize()
 	return ''.join(substrings)
 
+
+
 def translate_code(code, target_language):
 	tree = get_tree(code)
 	id_list, str_lit_list = set([]), set([])
 	get_translatables(tree, id_list, str_lit_list)
-	src_target_map = get_target(id_list, str_lit_list, target_language)
+	comment_list = get_comments(tree)
+	src_target_map = get_target(id_list, str_lit_list, comment_list, target_language)
 	translated_code = src_to_target(code, src_target_map)
-
-	# translate commentary
-
 
 	return translated_code
 
 def main():
 	code = u'''
+	// here's a leading comment
 	var greeting = "Hello, my friends!"
 	function fillPothole() {
 	   turnRight();
@@ -100,11 +119,12 @@ def main():
 	   move();
 	   turnRight();
 	}
+	/* here's a trailing comment */
 	'''
 	target_language = 'ja'
 
 	translated_code = translate_code(code, target_language)
-	import pdb;pdb.set_trace()
+	#import pdb;pdb.set_trace()
 	print("Original code: {}".format(code))
 	print("Translated code: {}".format(translated_code))
 
